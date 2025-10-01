300d6c5be3103b8a77920aa2caa29995a68ed0a4
muqeet1001 <abdul1002muqeet@gmail.com>
2025-10-01 21:27:08 +0530

    notes

--- Day 1/server.js
+++ Day 1/server.js
@@ -1,9 +1,10 @@
 const app = require("express");
 const server = app();
+server.use(app.json());
+server.post('/notes',(req,res)=>{
+  console.log(req.body)
+});
  
-server.get("/home", (req,res)=>{
-    res.send("Welcome to home page")
-})
 
 server.listen(3000,()=>{
     console.log("server is runing in port 3000");
