949fc0406e8184a8555850712d6f7a6f513b1051
muqeet1001 <abdul1002muqeet@gmail.com>
2025-10-02 22:54:22 +0530

    CRUD in server

---
+++ Day 2/Project/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+let notes =[];
+app.post('/notes',(req,res)=>{
+    const body = req.body;
+     notes.push(body);
+     res.json({
+        message:"note created succefully"
+     })
+})
+
+app.get('/notes',(req,res)=>{
+    res.json(notes);
+})
+
+app.delete('/notes/:index' ,(req,res)=>{
+    const index = req.params.index;
+    delete notes[index];
+    res.send("Deleter succefully");
+})
+
+app.patch('/notes/:index',(req,res)=>{
+   const index = req.params.index;
+   const {title} = req.body;
+   notes[index].title = title;
+   res.send("updates succesfully");
+})
+
+app.listen(3000, () => {
+  console.log("server is running in port 3000");
+});
