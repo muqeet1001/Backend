04a04d6c8729225931553e4c2da8748af0f60c2b
muqeet1001 <abdul1002muqeet@gmail.com>
2025-10-04 22:42:03 +0530

    create and read opt done

--- Day 4/index.js
+++ Day 4/index.js
@@ -1,9 +1,26 @@
-const express = require('express');
-const connectToDB = require('./src/db/db');
-
+const express = require("express");
+const connectToDB = require("./src/db/db");
+const noteModel = require("./src/models/note.model");
 connectToDB();
 const app = express();
+app.use(express.json());
+app.post("/notes", async (req, res) => {
+  const { title, content } = req.body;
+  await noteModel.create({
+    title,
+    content,
+  });
+  res.json({
+    msg: "note is created and stored in db",
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("server is running in port 3000");
+app.get('/notes',async(req,res)=>{
+    const notes =  await noteModel.find();
+    res.json({
+        notes
+    })
 })
+app.listen(3000, () => {
+  console.log("server is running in port 3000");
+});
